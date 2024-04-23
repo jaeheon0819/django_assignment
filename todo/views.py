a63d2b442d2ad2efdc2fe80fc3e71efcdec6310b
jaeheon0819 <jhjhjh0819@naver.com>
2024-04-23 19:16:43 +0845

    add security to code

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Task
+
+def task_list(request):
+    tasks = Task.objects.all()
+    return render(request, 'todo/task_list.html', {'tasks': tasks})
+
+def add_task(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        Task.objects.create(title=title)
+        return redirect('task_list')
+    return render(request, 'todo/add_task.html')
 
 # Create your views here.
